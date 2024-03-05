53e2146c591d62d5f121cb57a6c9f18234974115
sofiahualda <s.hualda.2023@alumnos.urjc.es>
2024-03-05 16:43:08 +0100

    This is going to be my first but not last commit

--- S10/echo-server1.py
+++ S10/echo-server1.py
@@ -3,10 +3,11 @@ import termcolor
 
 # Configure the Server's IP and PORT
 PORT = 8080
-IP = "10.1.152.112"  # this IP address is local, so only requests from the same machine are possible
+IP = "212.128.255.87"  # this IP address is local, so only requests from the same machine are possible
 
 # -- Step 1: create the socket
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # -- Step 2: Bind the socket to server's IP and PORT
 ls.bind((IP, PORT))
@@ -15,19 +16,16 @@ ls.bind((IP, PORT))
 ls.listen()
 
 print("The server is configured!")
-
+print("Waiting for clients to connect")
 
 while True:
     (rs, address) = ls.accept()
-    print("Waiting for clients to connect")
     print("A client has connected to the server!")
 
     msg = rs.recv(2048).decode("utf-8")
-    msg_in_color = termcolor.cprint(msg, 'green')
-    print(msg_in_color)
-    print("Message received: ", msg_in_color)
+    print("Message received: " + termcolor.colored(msg, 'green'))
 
-    newflag = "I am a happy server"
+    newflag = "ECHO: " + msg
     rs.send(newflag.encode())
     rs.close()  # to close the socket of the client
 
