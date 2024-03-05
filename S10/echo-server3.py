b81c62959375458a4905c3a825532362c4a30ec0
sofiahualdaaguado <s.hualda.2023@alumnos.urjc.es>
2024-03-05 23:58:10 +0100

    helloooo

--- S10/echo-server3.py
+++ S10/echo-server3.py
@@ -5,31 +5,40 @@ import termcolor
 PORT = 8080
 IP = "192.168.1.34"  # this IP address is local, so only requests from the same machine are possible
 
-# -- Step 1: create the socket
+
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# -- Step 2: Bind the socket to server's IP and PORT
 ls.bind((IP, PORT))
-
-# -- Step 3: Configure the socket for listening
 ls.listen()
 
 print("The server is configured!")
 
 i = 1
+num_con = 0
+list_clients = []
 while True:
     (rs, address) = ls.accept()
     print("Waiting for clients to connect")
     print(f"Connection", i, "Client IP, PORT: ", address)
-    i += 1
+
+
+    num_con += 1
+    list_clients.append(address)
+
+
     msg = rs.recv(2048).decode("utf-8")
     print("Message received: " + termcolor.colored(msg, 'green'))
 
-
     newflag = "ECHO: " + msg
     rs.send(newflag.encode())
-    rs.close()  # to close the socket of the client
 
-# -- Close the socket
+    index_list = 0
+    if num_con == 5:
+        for element in list_clients:
+            index_list += 1
+            print(f"Client", index_list, element)
+    else:
+        rs.close()
+    i += 1
+
 ls.close()
--- S10/happy-server.py
+++ S10/happy-server.py
@@ -2,7 +2,7 @@ import socket
 
 # Configure the Server's IP and PORT
 PORT = 8080
-IP = "10.1.152.112"  # this IP address is local, so only requests from the same machine are possible
+IP = "192.168.1.34"  # this IP address is local, so only requests from the same machine are possible
 
 # -- Step 1: create the socket
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
