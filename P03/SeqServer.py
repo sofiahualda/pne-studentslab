ee68d058bd81a25a3ffc7b632edae7e993b30f60
sofiahualdaaguado <s.hualda.2023@alumnos.urjc.es>
2024-03-08 21:28:47 +0100

    helloooo

--- P03/SeqServer.py
+++ P03/SeqServer.py
@@ -4,57 +4,58 @@ import socket
 import termcolor
 
 
-PORT = 8080
-IP = "127.0.0.1"
+class Server:
+    def __init__(self):
+        PORT = 8080
+        IP = "127.0.0.1"
+        ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ls.bind((IP, PORT))
+        ls.listen()
 
+        print("Seq server configured!")
+        while True:
+            print(f"Waiting for clients...")
+            (rs, address) = ls.accept()
+            msg = rs.recv(2048).decode("utf-8")
+            response = self.what_appears_on_clients(msg)
+            send_bytes = str.encode(response)
+            rs.send(send_bytes)
+            rs.close()
+    def what_appears_on_clients(self, msg):
+        if msg == "PING":
+            print("Ping command!\nOK!")
+            return "OK"
 
-ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ls.bind((IP, PORT))
-ls.listen()
-
-print("Seq server configured!")
-print(f"Waiting for clients...")
-while True:
-    (rs, address) = ls.accept()
-    msg = rs.recv(2048).decode("utf-8")
-
-    if msg == "PING":
-        print("Ping command!\nOK!")
-        newflag = "OK!"
-        rs.send(newflag.encode())
-        print(f"\nWaiting for clients...")
-
-
-    GENES = ["U5", "RNU6_269P", "FXN", "FRAT1", "ADA"]
-    number = 0
-    for gene in GENES:
-        s = Seq()
-        s.read_fasta(os.path.join("..", "sequences", gene + ".txt"))
-        number += 1
-        if 0 <= number <= 4:
-            if msg == f"GET {number}":
-                termcolor.cprint("GET", 'green')
-                gene = str(s)
-                print(gene)
-                newflag = str(s)
-                rs.send(newflag.encode())
-                print(f"\nWaiting for clients...\n")
-
-
-
-
+        elif msg.startswith("GET"):
+            GENES = ["U5", "RNU6_269P", "FXN", "FRAT1", "ADA"]
+            number = -1
+            for gene in GENES:
+                s = Seq(gene)
+                s.read_fasta(os.path.join("..", "sequences", gene + ".txt"))
+                number += 1
+                index = GENES.index(gene)
+                if 0 <= number <= 4:
+                    if msg == f"GET {index}":
+                        termcolor.cprint("GET", 'green')
+                        gene = str(s)
+                        print(gene)
+                        return gene
 
 
 
-
+server = Server()
 
 
 
 
-    rs.close()
 
 
 
-    # -- Close the socket
-ls.close()
+
+
+
+
+
+
+
 
